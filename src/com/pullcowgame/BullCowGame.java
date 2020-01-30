d08ba8cfe5c5bc969c0ae4fc23267a7127018fd7
ahmedfou <ahmedfou2014@gmail.com>
2020-01-30 14:38:27 +0100

    add gameIntro

---
+++ src/com/pullcowgame/BullCowGame.java
@@ -0,0 +1,5 @@
+package com.pullcowgame;
+
+public class BullCowGame {
+
+}
--- src/com/pullcowgame/Main.java
+++ src/com/pullcowgame/Main.java
@@ -1,8 +1,35 @@
 package com.pullcowgame;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String args[]) {
-        System.out.println("Hello Pull Cow Game");
+    public static void gameIntro() {
+        System.out.println("Welcome to the Bulls and Cows game! ");
+        System.out.println("          }   {         ___ " );
+        System.out.println("          (o o)        (o o) ");
+        System.out.println( "   /-------\\ /          \\ /-------\\ ");
+        System.out.println("  / | BULL |O            O| COW  | \\ ");
+        System.out.println(" *  |-,--- |              |------|  * ");
+        System.out.println( "    ^      ^              ^      ^ " );
+        System.out.print("Can you guess the ");
+        System.out.println(" letter isogram I'm thinking of?\n");
+        return;
+    }
+
+    public static boolean replay()
+    {
+        System.out.print( "Replay? (y/n)");
+        Scanner sc = new Scanner(System.in);
+        char isReplaying = sc.nextLine().charAt(0);
+        return (isReplaying == 'y' || isReplaying == 'Y');
+    }
+
+
+    public static void main(String[] args) {
+        do {
+            gameIntro();
+        } while(replay());
+        return;
     }
 }
