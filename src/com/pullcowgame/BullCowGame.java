dbd6609697c5f06d0d42e9d53d0e8406f2e512e6
ahmedfou <ahmedfou2014@gmail.com>
2020-01-30 17:32:27 +0100

    .

--- src/com/pullcowgame/BullCowGame.java
+++ src/com/pullcowgame/BullCowGame.java
@@ -22,19 +22,28 @@ public class BullCowGame {
     }
 
 
-    public final EGuessStatus checkGuessValidity(String s) {
-        return null;
+    public final EGuessStatus checkGuessValidity(String guess) {
+      return null
     }
 
     public void reset() {
+        final String HIDDEN_WORD = "plane"; // this MUST be an isogram
+        myHiddenWord = HIDDEN_WORD;
 
+        myCurrentTry = 1;
+        isGameWon = false;
+        return;
     }
 
-    private final String isIsogram(String s) {
+    public BullCowCount SubmitValidGuess(String guess) {
         return null;
     }
 
-    private final String isLowerCase(String s) {
+    private final boolean isIsogram(String Word) {
+        return false;
+    }
+
+    private final String isLowerCase(String word) {
         return null;
     }
 }
